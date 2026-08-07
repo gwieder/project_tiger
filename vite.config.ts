8f79c9b54deaec558d842682ba4c66506997cd51
gwieder <gabewieder@gmail.com>
2026-08-06 22:45:04 -0400

    Update vite.config.ts
    underscore

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // This MUST match your GitHub repo name exactly, wrapped in slashes.
   // If you name the repo something other than "project-tiger", change this to match.
-  base: '/project-tiger/',
+  base: '/project_tiger/',
 })
